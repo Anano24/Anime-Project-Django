9587d157d202c540eef29da50534c79c86c98179
Anano24 <ananorobakidze24@gmail.com>
2024-07-01 23:14:34 +0400

    Add search functionality and genre filter - Implement search functionality in the navbar to allow users to search for anime titles. - Add a dropdown menu in the navbar to filter anime by genres. - Update navbar styles for better alignment and presentation. - Ensure search and filter functions work together seamlessly.
    This update enhances user experience by making it easier to find and filter anime content.

--- myanimeproject/anime/views.py
+++ myanimeproject/anime/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render
-from .models import Anime, User
+from .models import Anime, User, Genre
+from django.db.models import Q
 # Create your views here.
 
 
 
 
 def home(request):
-    anime_list = Anime.objects.all()
-    context = {'anime_list': anime_list}
+    query = request.GET.get("query")
+    genres = Genre.objects.all()
+
+    if query:
+        anime_list = Anime.objects.filter(Q(title__icontains=query) | Q(description__icontains=query) | Q(genres__name__icontains=query))
+        anime_list = list(set(anime_list))
+    else:
+        anime_list = Anime.objects.all()
+
+    heading = "All Animes"
+    context = {'anime_list': anime_list, 'heading': heading, 'genres': genres}
 
     return render(request, 'anime/home.html', context)
 
 
+
 def profile(request, pk):
     user = User.objects.get(id=int(pk))
-    animes = user.animes.all()
-    context = {'anime_list': animes, 'user': user}
+    query = request.GET.get("query")
+    genres = Genre.objects.all()
+
+    if query:
+        animes = user.animes.filter(Q(title__icontains=query) | Q(description__icontains=query) | Q(genres__name__icontains=query))
+        animes= list(set(animes))
+    else:
+        animes = user.animes.all()
+        
+    heading = "My Animes"
+    context = {'anime_list': animes, 'user': user, 'heading': heading, 'genres': genres}
     return render(request, 'anime/profile.html', context)
